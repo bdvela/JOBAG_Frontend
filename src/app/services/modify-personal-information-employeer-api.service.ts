cebb485ee70c8b628eddab9782e30fabf1cadffb
Bryan Vela A <55074395+bdvela@users.noreply.github.com>
2021-10-27 15:45:14 -0500

    Update modify-personal-information-employeer-api.service.ts

--- src/app/services/modify-personal-information-employeer-api.service.ts
+++ src/app/services/modify-personal-information-employeer-api.service.ts
@@ -12,7 +12,7 @@ export class ModifyPersonalInformationEmployeerApiService {
   constructor(private http: HttpClient) {
   }
 
-  private url: string = "https://jobagapi.herokuapp.com/api/users";
+  private url: string = "https://jo-ba-gapi.herokuapp.com/api/users";
 
   httpOptions = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
 
