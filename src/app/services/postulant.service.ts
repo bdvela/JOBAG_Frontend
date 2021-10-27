2cce039e9d46c6c86c88716357ac3a69b7b9eae1
Bryan Vela A <55074395+bdvela@users.noreply.github.com>
2021-10-27 15:46:37 -0500

    Update postulant.service.ts

--- src/app/services/postulant.service.ts
+++ src/app/services/postulant.service.ts
@@ -12,7 +12,7 @@ export class PostulantService {
   constructor(private http: HttpClient) {
   }
 
-  private url: string = "https://jobagapi.herokuapp.com/api/postulants";
+  private url: string = "https://jo-bag-api.herokuapp.com/api/postulants";
 
   httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
 // API Error Handling
